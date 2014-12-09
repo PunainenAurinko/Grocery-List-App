bd44dac894866f029a136b8bf80ebf8056f3ea4b
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2014-12-09 00:20:18 -0500

    App finalized

--- js/grocery.js
+++ js/grocery.js
@@ -1,19 +1,19 @@
 var myList = [];
 
-$(document).ready(function (ev){
+$(document).ready(function(ev) {
       
-    if(localStorage["grocery-tonk0006"]){
+    if(localStorage["grocery-tonk0006"]) {
         myList = JSON.parse(localStorage["grocery-tonk0006"]);
     }
     
     showList();
   
-    $("#addItem").click(function(ev){
+    $("#addItem").click(function(ev) {
         ev.preventDefault();
         var newItem = document.querySelector("#item").value;
             if (newItem !== ""){
-                newItem = newItem + ":false";
-            myList.push( newItem );
+                newItem = newItem + ":true";
+            myList.push(newItem);
             }
         localStorage["grocery-tonk0006"] = JSON.stringify(myList);
         document.querySelector("#myForm").reset();
@@ -26,35 +26,31 @@ function markAsDone() {
     $(this).toggleClass("strikethrough");
     var selectedText = $(this).text();
     if(localStorage["grocery-tonk0006"]){
-        for (var i = 0; i<myList.length; i++) {
+        for (var i = 0; i < myList.length; i++) {
             var parts = myList[i].split(":");
             
-            if(parts[0]==selectedText){
+            if(parts[0] == selectedText) {
                 
-                if(parts[1]=="false") 
-                {
-                    parts[1]="true";
+                if(parts[1] == "true") {
+                    parts[1] = "false";
                 }
-                else if(parts[1]=="true")
-                {
-                    parts[1]="false";
+                else if(parts[1] == "false") {
+                    parts[1] = "true";
                 }
                 
-                myList[i] = parts[0]+":"+parts[1];
+                myList[i] = parts[0] + ":" + parts[1];
             }
         }
     }
     localStorage["grocery-tonk0006"] = JSON.stringify(myList);
-    //console.log(localStorage["grocery-tonk0006"]);
 }
 
 function removeItem(ev){
     var txt = ev.currentTarget.firstChild.nodeValue;
-    for(var i=0; i<myList.length; i++){
+    for(var i = 0; i < myList.length; i++){
         var parts = myList[i].split(":");
         if(parts[0] == txt){
-        // found the match
-        myList.splice(i, 1);
+            myList.splice(i, 1);
         }
     }
     localStorage["grocery-tonk0006"] = JSON.stringify(myList);
@@ -64,12 +60,12 @@ function removeItem(ev){
 function showList(){
     var output = document.querySelector("#itemList");
     output.innerHTML = "";
-    for(var i=0; i<myList.length; i++){
+    for(var i = 0; i < myList.length; i++){
         var list = document.createElement("li");
         var parts = myList[i].split(":");
         list.innerHTML = parts[0];
         
-        if(parts[1]=="true"){
+        if(parts[1] == "false") {
             $(list).toggleClass("strikethrough");
         }
         output.appendChild(list);
